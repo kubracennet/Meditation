bdbc7209cf937c13d69e11995f81f5a614abc835
kubracennet <kubracennetyavasoglu@gmail.com>
2023-04-08 01:09:48 +0300

    Create Home Page

--- Meditation/ContentView.swift
+++ Meditation/ContentView.swift
@@ -14,16 +14,103 @@ struct ContentView: View {
     }
 }
 
+struct HomeTabView: View {
+    var body: some View {
+        ScrollView (.vertical, showsIndicators: false) {
+            VStack (alignment: .leading) {
+                Text("Hello, Darling!").font(.title).bold()
+                HStack {
+                    Text("Discover Plan").padding()
+                    Spacer()
+                    Image(systemName: "magnifyingglass").foregroundColor(Color(UIColor.systemGray)).padding()
+                }.foregroundColor(Color.secondary.opacity(0.5)).background(Color.gray.opacity(0.1)).cornerRadius(8).shadow(radius: 1)
+                VStack (alignment: .leading) {
+                    Text("Ready to Continue?").font(.title).bold()
+                    Text("Continue your personal course")
+                }.padding(.vertical)
+                ZStack {
+                    VStack {
+                        HStack {
+                            VStack (alignment: .leading) {
+                                Text("Basics").font(.title).fontWeight(.medium)
+                                Text("Day 4 of 10").font(.footnote)
+                            }
+                            Spacer()
+                            Text("20 min").padding(.all, 6).background(Color.white).clipShape(Capsule())
+                            
+                        }
+                        HStack (alignment: .bottom) {
+                            Image("basic").resizable().aspectRatio(contentMode: .fill).frame(width: 250, height: 150)
+                            Spacer()
+                            Image(systemName: "play.fill").foregroundColor(.white).padding().background(Color.black).clipShape(Circle())
+                        }
+                    }
+                }.padding().background(Color(UIColor.systemOrange).opacity(0.4)).cornerRadius(24)
+                Text("For You").font(.title).bold().padding(.top)
+                ScrollView (.horizontal, showsIndicators: false) {
+                    HStack (spacing: 24) {
+                        Text("Sleep").bold().padding(.all, 15).foregroundColor(.white).background(Color.black).cornerRadius(18)
+                        
+                        Text("Inner Peace")
+                        Text("Stress")
+                        Text("Anxiety")
+                        Text("Anxiety")
+
+                    }
+                }
+                
+                HStack (spacing: 24) {
+                    VStack (spacing: 24) {
+                        VStack (alignment: .leading) {
+                            Text("Managing Anxiety").bold()
+                            Text("20 min").padding(.all, 6).background(Color.white)
+                                .clipShape(Capsule())
+                            
+                            Image("health").resizable().aspectRatio(contentMode: .fill)
+                        }.padding().frame(width: 165, height: 250).background(Color(UIColor.systemBlue).opacity(0.2)).cornerRadius(24)
+                        
+                        VStack (alignment: .leading) {
+                            Text("Managing Anxiety").bold()
+                            Text("20 min").padding(.all, 6).background(Color.white)
+                                .clipShape(Capsule())
+                            
+                            Image("travel").resizable().aspectRatio(contentMode: .fill)
+                        }.padding().frame(width: 165, height: 250).background(Color(UIColor.systemBlue).opacity(0.2)).cornerRadius(24)
+                      
+                      }
+                      VStack (spacing: 24) {
+                      VStack (alignment: .leading) {
+                        Text("Managing Anxiety").bold()
+                        Text("20 min").padding(.all, 6).background(Color.white)
+                            .clipShape(Capsule())
+                        
+                        Image("health").resizable().aspectRatio(contentMode: .fill)
+                    }.padding().frame(width: 165, height: 250).background(Color(UIColor.systemBlue).opacity(0.2)).cornerRadius(24)
+                    
+                     VStack (alignment: .leading) {
+                        Text("Managing Anxiety").bold()
+                        Text("20 min").padding(.all, 6).background(Color.white)
+                            .clipShape(Capsule())
+                        
+                        Image("travel").resizable().aspectRatio(contentMode: .fill)
+                    }.padding().frame(width: 165, height: 250).background(Color(UIColor.systemBlue).opacity(0.2)).cornerRadius(24)
+                   }
+                }.padding(.vertical)
+            }.padding()
+        }
+    }
+}
+
 struct HomeView: View {
     var body: some View {
         TabView {
-            Text("Home").tabItem {
+            HomeTabView().tabItem {
                 Image(systemName: "house")
             }
             Text("Tab#2").tabItem {
                 Image(systemName: "heart")
             }
-            Text("Tab#3").tabItem {
+            Text("").tabItem {
                 Image(systemName: "magnifyingglass")
             }
         }
